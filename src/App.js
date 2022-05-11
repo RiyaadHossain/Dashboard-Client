76449df385449c6299ae844dd2788d808bc7b7f6
Riyaad_Hossain <riyadhossain.dev@gmail.com>
2022-05-11 20:16:12 +0600

    dependecies added and components set up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Home from './Components/Home/Home';
+import Products from './Components/Products/Products';
+import AddProducts from './Components/AddProducts/AddProducts';
+import DashBoard from './Components/DashBoard/DashBoard';
 
 function App() {
   return (
     <div>
-      
+      <DashBoard/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/products' element={<Products/>}/>
+        <Route path='/addproduct' element={<AddProducts/>}/>
+      </Routes>
     </div>
   );
 }
