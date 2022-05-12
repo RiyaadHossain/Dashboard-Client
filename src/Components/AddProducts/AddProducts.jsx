99e9bf9cc376ba6873c23af8992f76da1bf81a63
Riyaad_Hossain <riyadhossain.dev@gmail.com>
2022-05-12 21:38:18 +0600

    pagination working

--- src/Components/AddProducts/AddProducts.jsx
+++ src/Components/AddProducts/AddProducts.jsx
@@ -1,34 +1,36 @@
-import toast, { Toaster } from 'react-hot-toast';
+import toast, { Toaster } from "react-hot-toast";
 import axios from "axios";
 import React from "react";
 
 const AddProducts = () => {
-
-    const handleAddProduct = async e => {
-        e.preventDefault()
-        const product = {
-            name: e.target.product.value,
-            price: e.target.price.value,
-            imgUrl: e.target.imgUrl.value
-        }
-        const { data } = await axios.post('http://localhost:5000/product', product)
-        if(!data.success) return toast.error(data.error)
-        toast.success(data.message)
-        e.target.reset()
-    }
-
-
+  const handleAddProduct = async (e) => {
+    e.preventDefault();
+    const product = {
+      name: e.target.product.value,
+      price: e.target.price.value,
+      imgUrl: e.target.imgUrl.value,
+    };
+    const { data } = await axios.post("http://localhost:5000/product", product);
+    if (!data.success) return toast.error(data.error);
+    toast.success(data.message);
+    e.target.reset();
+  };
 
   return (
     <div className="container mx-auto">
-        <Toaster />
-        <h3 className='text-center mt-12 text-3xl font-semibold text-blue-600'>Add A New Product</h3>
-      <form onSubmit={handleAddProduct} className=" border-2 p-6 mt-12 rounded border-zinc-600">
+      <Toaster />
+      <h3 className="text-center mt-12 text-3xl font-semibold text-blue-600">
+        Add A New Product
+      </h3>
+      <form
+        onSubmit={handleAddProduct}
+        className=" border-2 p-6 mt-12 w-[500px] mx-auto rounded border-zinc-600"
+      >
         <div className="relative z-0 w-full mb-6 group">
           <input
             type="text"
             name="product"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500  focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
             required=""
           />
@@ -72,15 +74,13 @@ const AddProducts = () => {
           </label>
         </div>
 
-        <button 
-        
+        <button
           type="submit"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Add Product
         </button>
       </form>
-      
     </div>
   );
 };
--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -5,73 +5,105 @@ import toast, { Toaster } from "react-hot-toast";
 const Products = () => {
   const [products, setProducts] = useState([]);
   const [isDeleted, setIsDeleted] = useState(false);
+  const [limit, setLimit] = useState(5);
+  const [pageNumber, setPageNumber] = useState(0);
+
   useEffect(() => {
     // IFFE
     (async () => {
-      const { data } = await axios.get("http://localhost:5000/products");
-      console.log(data);
+      const { data } = await axios.get(
+        `http://localhost:5000/products?limit=${limit}&pageNumber=${pageNumber}`
+      );
       if (!data?.success) return toast.error(data?.error);
 
       setProducts(data.data);
     })();
-  }, [isDeleted]);
-
-  const handleDelete = async id => {
-    const { data } = await axios.delete(`http://localhost:5000/product/${id}`)
-    if(data.success) toast.success(data.message)
-    setIsDeleted(!isDeleted)
-}
+  }, [isDeleted, limit, pageNumber]);
+  console.dir(limit, pageNumber);
+  const handleDelete = async (id) => {
+    const { data } = await axios.delete(`http://localhost:5000/product/${id}`);
+    if (data.success) toast.success(data.message);
+    setIsDeleted(!isDeleted);
+  };
   return (
-    <div className="mx-auto container my-12 border-2 border-slate-500 rounded-md">
-      <Toaster />
+    <>
+      <div className="mx-auto container my-12 border-2 border-slate-500 rounded-md">
+        <Toaster />
 
-      <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-          <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-              <th scope="col" class="px-6 py-3">
-                Image
-              </th>
-              <th scope="col" class="px-6 py-3">
-                Product name
-              </th>
-              <th scope="col" class="px-6 py-3">
-                Price
-              </th>
-              <th scope="col" class="px-6 py-3">
-                <span class="sr-only">Edit</span>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {products.map((product) => (
-              <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <th
-                  scope="row"
-                  class="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
-                >
-                  <img
-                    className="w-16 h-16 rounded"
-                    src={product.imgUrl}
-                    alt=""
-                  />
+        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+              <tr>
+                <th scope="col" className="px-6 py-3">
+                  Image
                 </th>
-                <td class="px-6 py-4">{product.name}</td>
-                <td class="px-6 py-4">{product.price}</td>
-                <td class="px-6 py-4 text-right">
-                  <button
-                    onClick={() => handleDelete(product._id)}
-                    class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                  >
-                    Delete
-                  </button>
-                </td>
+                <th scope="col" className="px-6 py-3">
+                  Product name
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  Price
+                </th>
+                <th scope="col" className="px-6 py-3">
+                  <span className="sr-only">Edit</span>
+                </th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {products.map((product, i) => (
+                <tr
+                  key={i}
+                  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+                >
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 dark:text-white whitespace-nowrap"
+                  >
+                    <img
+                      className="w-16 h-16 rounded"
+                      src={product.imgUrl}
+                      alt=""
+                    />
+                  </th>
+                  <td className="px-6 py-4">{product.name}</td>
+                  <td className="px-6 py-4">{product.price}</td>
+                  <td className="px-6 py-4 text-right">
+                    <button
+                      onClick={() => handleDelete(product._id)}
+                      className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
+      <div className="text-center">
+        {[...Array(5).keys()].map((page) => (
+          <button
+            onClick={() => setPageNumber(page)}
+            className={`mx-5 border-2 border-blue-600 py-1 font-bold px-3 ${
+              page === pageNumber ? "bg-blue-600 text-white" : ""
+            }`}
+          >
+            {page + 1}
+          </button>
+        ))}
+      <select
+        id="countries"
+        onChange={(e) => setLimit(e.target.value)}
+        class="bg-gray-50 border w-16 font-bold inline-block border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      >
+        <option value="5">5</option>
+        <option value="10">10</option>
+        <option value="15">15</option>
+        <option value="20">20</option>
+      </select>
+      </div>
+
+    </>
   );
 };
 
